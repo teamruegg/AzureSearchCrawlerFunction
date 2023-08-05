757a56a16873dcf5727361a1717ec73ffdfbb0f7
urruegg <urs.ruegg@microsoft.com>
2023-08-05 15:37:18 +0200

    Add MultiResponse

--- AzureSearchCrawlerFunction.cs
+++ AzureSearchCrawlerFunction.cs
@@ -5,6 +5,14 @@ using Microsoft.Extensions.Logging;
 
 namespace Teamruegg.AzureSearchCrawler
 {
+
+    public class MultiResponse
+    {
+        [QueueOutput("outqueue",Connection = "AzureWebJobsStorage")]
+        public string[] Messages { get; set; }
+        public HttpResponseData HttpResponse { get; set; }
+    }
+
     public class AzureSearchCrawlerFunction
     {
         private readonly ILogger _logger;
@@ -15,16 +23,26 @@ namespace Teamruegg.AzureSearchCrawler
         }
 
         [Function("AzureSearchCrawlerFunction")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        //ori: public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+         public MultiResponse Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req, FunctionContext executionContext)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+    
+            var logger = executionContext.GetLogger("HttpExample");
+            logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var message = "Welcome to Azure Functions!";
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString(message);
 
-            response.WriteString("Welcome to Azure Functions!");
-
-            return response;
+            // Return a response to both HTTP trigger and storage output binding.
+            return new MultiResponse()
+            {
+                // Write a single message.
+                Messages = new string[] { message },
+                HttpResponse = response
+            };
         }
     }
 }
